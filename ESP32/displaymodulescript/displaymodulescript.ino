b215df57c70c551c4ef4be28202b17e2e463a5d2
Xavier Santos <miguel@xaviersantos.dev>
2023-06-25 20:20:19 +0100

    esp32 final

--- ESP32/displaymodulescript/displaymodulescript.ino
+++ ESP32/displaymodulescript/displaymodulescript.ino
@@ -6,11 +6,14 @@ const char* ssid = "iPhone de Xavier (2)";
 const char* password = "pequena132";
 const char* mqttServer = "172.20.10.6";
 const int mqttPort = 1883;
-const char* mqttTopic = "pz1_display_value";
+const char* mqttSetTopic = "pz1_set_topic";
+const char* mqttValueTopic = "pz1_display_value";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 SevSeg sevseg;
+
+bool isTopicSet = false;
 
 void setup() {
   // Connect to Wi-Fi
@@ -37,11 +40,23 @@ void setup() {
   // Connect to MQTT Broker
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
-  
+
+  Serial.println("Connecting to MQTT Broker...");
+
+  while (!client.connected()) {
+    if (client.connect("ESP32Client")) {
+      Serial.println("MQTT connected");
+      client.subscribe(mqttSetTopic);
+    } else {
+      Serial.print("MQTT connection failed, retrying in 5 seconds...");
+      delay(5000);
+    }
+  }
+
   // Initialize the seven-segment display
   byte sevenSegments = 1;
   byte CommonPins[] = {};
-  byte LEDsegmentPins[] = {13, 12, 14, 27, 26, 25, 33};
+  byte LEDsegmentPins[] = {13, 32, 14, 27, 26, 25, 33};
   bool resistorsOnSegments = true;
   sevseg.begin(COMMON_ANODE, sevenSegments, CommonPins, LEDsegmentPins, resistorsOnSegments);
   sevseg.setBrightness(80);
@@ -52,15 +67,25 @@ void loop() {
     reconnect();
   }
   client.loop();
-} 
+}
 
 void callback(char* topic, byte* payload, unsigned int length) {
   // Convert the received payload to a string
   payload[length] = '\0';
   String message = String((char*)payload);
-  
-  // Check if the received topic matches the subscribed topic
-  if (String(topic) == mqttTopic) {
+
+  // Check if the received topic matches the set topic
+  if (String(topic) == mqttSetTopic) {
+    // Update the mqttValueTopic with the new topic
+    mqttValueTopic = message.c_str();
+    isTopicSet = true;
+    client.unsubscribe(mqttSetTopic);
+    client.subscribe(mqttValueTopic);
+    Serial.print("New MQTT topic set: ");
+    Serial.println(mqttValueTopic);
+  }
+  // Check if the received topic matches the subscribed value topic
+  else if (isTopicSet && String(topic) == mqttValueTopic) {
     // Update the display with the received value
     int value = message.toInt();
     Serial.println(message);
@@ -74,7 +99,11 @@ void reconnect() {
     Serial.print("Attempting MQTT connection...");
     if (client.connect("ESP32Client")) {
       Serial.println("connected");
-      client.subscribe(mqttTopic);
+      if (!isTopicSet) {
+        client.subscribe(mqttSetTopic);
+      } else {
+        client.subscribe(mqttValueTopic);
+      }
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
